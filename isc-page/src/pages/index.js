6658206b12ef5c525ec5bd25d2a9a8efb443e902
Luis Anaya <luis.ernesto.anaya@upa.edu.mx>
2021-07-30 20:47:48 -0500

    bootstrap

---
+++ isc-page/src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from "react-bootstrap";
+
+
+ function Home() {
+  return ( 
+  <div>
+    Hello world!
+    <Button>Boton</Button>
+    </div>
+    
+    )
+}
+export default Home;
