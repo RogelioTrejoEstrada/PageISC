94fdd08f2424e25b7ee84ac942c9d440d8d9db27
Luis Anaya <luis.ernesto.anaya@upa.edu.mx>
2021-08-27 19:47:13 -0500

    Formato a pagina 404

--- isc-page/src/pages/404.js
+++ isc-page/src/pages/404.js
@@ -1,10 +1,50 @@
 import React from 'react'
+import { Col, Container, Row} from 'react-bootstrap'
+import Cabecera from '../components/Cabecera'
+import Layout from '../components/Layout'
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import { StaticImage } from 'gatsby-plugin-image'
 
-export default function NotFound() {
+
+export default function NotFound({data}) {
     return (
-        <div>
-            <h2> Page doesn't exists</h2>
-            <h3> Page doesn't exists</h3>
-        </div>
+      <Layout>
+        <div className="instalaciones"></div>
+        <Cabecera
+          titulo2="Página no encontrada"
+          titulo1="Ingeniería en Sistemas Computacionales"
+          imagen={data.file.childImageSharp.fluid}
+        />
+        <Container className="mb-5">
+          <Row className="justify-content-md-center">
+            <Col md={6} className="m-auto">
+              <StaticImage src="../images/inicio.jpg" alt="UPA Logo" />
+            </Col>
+            <Col md={6}>
+              <div className="textoJustificado">
+                <h3>Página no encontrada</h3>
+                <p>
+                  La página a la que ingresaste no existe, favor de visitar alguna que se encuentre en 
+                  el menú superior.
+                </p>
+                
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </Layout>
     )
 }
+
+export const query = graphql`
+ query banner404{
+   file(relativePath: {eq: "inicio.jpg"}){
+     childImageSharp {
+       fluid(maxWidth: 1800) {
+         ...GatsbyImageSharpFluid
+       }
+     }
+   }
+ }
+`;
