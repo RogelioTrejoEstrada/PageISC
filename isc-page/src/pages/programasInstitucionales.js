cd6bfe7e3e6a11b58ccc06d5193098f3a9f8d2e6
wicho <luis.ernesto.anaya@upa.edu.mx>
2022-02-28 15:15:03 -0600

    acordion en programas

--- isc-page/src/pages/programasInstitucionales.js
+++ isc-page/src/pages/programasInstitucionales.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { Col, Container, Row} from "react-bootstrap";
+import { Accordion, Col, Container, Row } from "react-bootstrap";
 import Layout from "../components/Layout";
 import Cabecera from "../components/Cabecera";
 import Seo from "../components/Seo";
@@ -8,47 +8,59 @@ import Seo from "../components/Seo";
 import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
 
-function Programas({data}) {
+function Programas({ data }) {
   const programasInstitucionales = data.programas.nodes
   console.log(programasInstitucionales)
   return (
     <Layout>
-      <Seo title = "Programas"/>
+      <Seo title="Programas" />
       <div className='programas'></div>
       <Cabecera
-          titulo2="Programas"
-          titulo1="Ingeniería en Sistemas Computacionales"
-          imagen = {data.banner.childImageSharp.fluid}
-        />
-      <Container className = 'mb-5'>
-        {programasInstitucionales.map((programa, index) => (
-          <Row className="justify-content-md-center mb-5" key={programa.id}>
-              <div className="textoJustificado">
-                <h3 className="text-center"> {programa.frontmatter.title}</h3>
-                <Row>
-                  
-                  <Col md={index % 2 === 0 ? {order: 1}: null}>
-                    <div dangerouslySetInnerHTML={{ __html: programa.html }} />
-                  </Col>
-                  
-                  {programa.frontmatter.thumb ? 
-                    <Col>
-                      <div className="image-contenedor">
-                      <Img
-                        fluid={programa.frontmatter.thumb.childImageSharp.fluid}
-                        alt={programa.frontmatter.stack}
-                        className="image-escala image-zoom"
-                        //style={{ height: "100%", width: "100%" }}
-                      />
-                      </div>
-                    </Col> : null}
+        titulo2="Programas"
+        titulo1="Ingeniería en Sistemas Computacionales"
+        imagen={data.banner.childImageSharp.fluid}
+      />
+      <Container className='mb-5'>
 
-                </Row>
-              </div>
+        <Accordion>
+          {programasInstitucionales.map((programa, index) => (
 
-           
-          </Row>
-        ))}
+
+            <Accordion.Item eventKey={index}>
+              <Row className="justify-content-md-center" key={programa.id}>
+
+                <Accordion.Header>
+                  <h5 className="text-center"> {programa.frontmatter.title}</h5>
+                </Accordion.Header>
+                <Accordion.Body>
+
+                  <Row className="textoJustificado">
+
+                    <Col md={index % 2 === 0 ? { order: 1 } : null}>
+                      <div dangerouslySetInnerHTML={{ __html: programa.html }} />
+                    </Col>
+
+                    {programa.frontmatter.thumb ?
+                      <Col>
+                        <div className="image-contenedor">
+                          <Img
+                            fluid={programa.frontmatter.thumb.childImageSharp.fluid}
+                            alt={programa.frontmatter.stack}
+                            className="image-escala image-zoom"
+                          //style={{ height: "100%", width: "100%" }}
+                          />
+                        </div>
+                      </Col> : null}
+                  </Row>
+                </Accordion.Body>
+
+              </Row>
+
+
+            </Accordion.Item>
+
+          ))}
+        </Accordion>
       </Container>
     </Layout>
   )
@@ -56,7 +68,7 @@ function Programas({data}) {
 
 export default Programas;
 
- export const query = graphql`
+export const query = graphql`
  query {
   banner: file(relativePath: {eq: "inicio.jpg"}){
      childImageSharp {
@@ -88,4 +100,3 @@ export default Programas;
      
  }
 `;
- 
