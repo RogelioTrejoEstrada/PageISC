e4a2a895c73f3831a27d4e441f67592821cad6a4
wicho <luis.ernesto.anaya@upa.edu.mx>
2022-03-07 08:19:10 -0600

    foto coworking

--- isc-page/src/components/ModalImagen.js
+++ isc-page/src/components/ModalImagen.js
@@ -5,24 +5,24 @@ export default function ModalImagen(props) {
 
  
 
-    return (
-        <Modal 
-          {...props}
-          size="lg"
+  return (
+    <Modal
+      {...props}
+      size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" >
-            Close
-          </Button>
-          <Button variant="primary" >
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    )
+      <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" >
+          Close
+        </Button>
+        <Button variant="primary" >
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  )
 }
