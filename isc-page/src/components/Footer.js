d1d1a34130dd00bbbbb2d81b2502d8a22acf152b
wicho <luis.ernesto.anaya@upa.edu.mx>
2022-07-26 23:28:55 -0500

    ajuste en footer

--- isc-page/src/components/Footer.js
+++ isc-page/src/components/Footer.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap';
 
-import {footer, copyright} from '../styles/Footer.module.css'
+//import '../styles/Footer.module.css'
 import {TelephoneFill, EnvelopeFill, GeoAltFill, Whatsapp, Facebook } from 'react-bootstrap-icons'
 import { Link } from 'gatsby';
 
 function Footer() {
     return (
-      <div className={footer}>
+      <div className="footer1">
         <Container>
           <Row className='mt-1 justify-content-center'>
             <Col md={4} className="ml-4">
               <h6 className='text-center footer-titulo'>Contacto</h6>
-              <ul className="list-unstyled pl-1">
+              <ul className="list-unstyled pl-1 footer-ul">
                 <li><span><GeoAltFill /></span> Calle Paseo San Gerardo 207, San Gerardo</li>
                 <li> C.P. 20342 Aguascalientes, Ags</li>
                 <li><span><EnvelopeFill /></span> isei@upa.edu.mx </li>
@@ -31,7 +31,7 @@ function Footer() {
               </ul>
             </Col>
             <Col md={4} className="ml-4">
-              <h6 className='text-center'>Sitios amigos</h6>
+              <h6 className='text-center footer-titulo'>Sitios amigos</h6>
               <ul className="list-unstyled pl-1">
                 <li><a href='https://www.facebook.com/ingenieria.isei' target='_blank' rel='noreferrer'><Facebook /> Ingeniería en Sistemas Computacionales</a> </li>
                 <li><a href='https://www.facebook.com/UniversidadPolitecnicaAguascalientes' target='_blank' rel='noreferrer'><Facebook /> Universidad Politécnica de Aguascalientes</a></li>
@@ -45,7 +45,7 @@ function Footer() {
           
           <Row > 
           <Col className="text-center">
-          <p className={copyright}>ISC - Universidad Politécnica de Aguascalientes</p>
+          <p className="copyright">ISC - Universidad Politécnica de Aguascalientes</p>
           </Col>
           </Row>
         </Container>
