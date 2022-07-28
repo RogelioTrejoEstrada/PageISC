30e090f3cf9b42131923a3195cc6ece65d67a9b0
wicho <luis.ernesto.anaya@upa.edu.mx>
2022-07-28 08:50:12 -0500

    quitando a mauricio

--- isc-page/src/pages/index.js
+++ isc-page/src/pages/index.js
@@ -91,7 +91,7 @@ export default function Home({ data }) {
             <LinkedInBadge user="esteban-uscanga-olea-862aba64" theme="light"/>
             {/* <LinkedInBadge user="ing-maria-isabel-huerta-escobar-b9747a13b" theme="light"/> */}
              {/* <LinkedInBadge user="paola-sánchez-gándara-5709411b9" theme="light"/> */}
-            <LinkedInBadge user="mauricioalcala" theme="light"/>
+            {/* <LinkedInBadge user="mauricioalcala" theme="light"/> */}
             {/*<LinkedInBadge user="jimena-velázquez-gómez-3703a7164" theme="light"/> */}
           </Grid>
           </Container>
