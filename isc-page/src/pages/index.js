67eed25a0785a2433f0161e96c0e8703e5ba5cc8
wicho <luis.ernesto.anaya@upa.edu.mx>
2022-07-28 08:46:02 -0500

    quitando a paola

--- isc-page/src/pages/index.js
+++ isc-page/src/pages/index.js
@@ -90,7 +90,7 @@ export default function Home({ data }) {
             <LinkedInBadge user="leonardo-daniel-arévalo-pérez-2235321b1" theme="light"/>
             <LinkedInBadge user="esteban-uscanga-olea-862aba64" theme="light"/>
             {/* <LinkedInBadge user="ing-maria-isabel-huerta-escobar-b9747a13b" theme="light"/> */}
-             <LinkedInBadge user="paola-sánchez-gándara-5709411b9" theme="light"/>
+             {/* <LinkedInBadge user="paola-sánchez-gándara-5709411b9" theme="light"/> */}
             <LinkedInBadge user="mauricioalcala" theme="light"/>
             {/*<LinkedInBadge user="jimena-velázquez-gómez-3703a7164" theme="light"/> */}
           </Grid>
